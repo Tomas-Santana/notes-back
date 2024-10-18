3527f66ce2d596f9063884f994d59851887e06f1
MarlonUrda <MGUR2025pro@gmail.com>
2024-10-17 23:38:49 -0400

    nuevas rutas agregadas

---
+++ api/resource/putUser.go
@@ -0,0 +1,24 @@
+package resource
+
+import (
+	"notes-back/types/requestTypes"
+	"github.com/gin-gonic/gin"
+)
+
+func(rg *ResourceGroup) PutUser(c *gin.Context) {
+	var req requestTypes.UpdateUser
+  if err := c.ShouldBindJSON(&req); err!= nil {
+    c.JSON(400, gin.H{"error": err.Error()})
+    return
+  }
+
+	err := rg.db.UpdateUser(&req)
+	if err!= nil {
+    c.JSON(500, gin.H{"error": "Failed to update user"})
+    return
+  }
+
+	c.JSON(200, gin.H{
+		"user": req.ID,
+	})
+}
