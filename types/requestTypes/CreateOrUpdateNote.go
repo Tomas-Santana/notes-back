fbec04f296d7260848a9d9474c8791d4a7f17907
Tomas Santana <tommysantana5@gmail.com>
2024-10-18 01:25:40 -0400

    changed content max

--- types/requestTypes/CreateOrUpdateNote.go
+++ types/requestTypes/CreateOrUpdateNote.go
@@ -8,7 +8,7 @@ import (
 type CreateNote struct {
 	ID         string `bson:"_id,omitempty" json:"_id"`
 	Title      string `json:"title" binding:"required,min=1,max=100"`
-	Content    string `json:"content" binding:"required,min=1,max=1000"`
+	Content    string `json:"content" binding:"required,min=1,max=500"`
 	Html       string `json:"html" binding:"required,min=3,max=1000"`
 	Importance int    `json:"importance" binding:"min=0,max=5"`
 	Categories []types.NoteCategory `json:"categories"`
@@ -17,7 +17,7 @@ type CreateNote struct {
 type UpdateNote struct {
 	ID         string  `bson:"_id,omitempty" json:"_id" binding:"required"`
 	Title      *string `json:"title" binding:"omitempty,min=1,max=100"`
-	Content    *string `json:"content" binding:"omitempty,min=1,max=1000"`
+	Content    *string `json:"content" binding:"omitempty,min=1,max=500"`
 	Html       *string `json:"html" binding:"omitempty,min=3,max=1000"`
 	IsFavorite *bool   `json:"isFavorite"`
 	Importance *int    `json:"importance" binding:"min=0,max=5"`
